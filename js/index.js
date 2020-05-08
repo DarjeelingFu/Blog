30f19c1f6cef908b5057a274766f794bbdf55be0
DarjeelingFu <57053832+DarjeelingFu@users.noreply.github.com>
2020-05-08 12:35:48 +0800

    blog

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#LOG').click(function(){
+		$('#logBox').fadeToggle('10');
+	});
+	$('#login button').click(function(){
+		$('#erInfo').fadeToggle('10');
+	});
+});
